58d22a2099145112113dc11002986c5612fd7eb1
Natasha K <kapr.natasha@gmail.com>
2018-06-13 14:40:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(keyboardkey){
+    if(keyboardkey.which === 71){
+      alert('You have pressed down on G!');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
